bdc099cbc47e728dff1921705825b97d95a4056a
Deluxe <zouhaodong@outlook.com>
2022-09-09 23:20:01 +0800

    修改通知条件，只有签到异常时通知

--- notify/notifier.py
+++ notify/notifier.py
@@ -15,12 +15,21 @@ class Notifier(object):
         self.qmesg_chan = qmsgchan.QmsgChan()
 
     def do_notify(self, sign_list, errmsg):
-        log.info('Starting notifying')
-        if self.config.Ding_SECRET != '' and self.config.Ding_WEBHOOK != '':
-            self.ding_bot.send(self.ding_bot.form_content(sign_list, self.config.DISP_TYPE), errmsg)
+        # log.info('Starting notifying')
+        # if self.config.Ding_SECRET != '' and self.config.Ding_WEBHOOK != '':
+        #     self.ding_bot.send(self.ding_bot.form_content(sign_list, self.config.DISP_TYPE), errmsg)
 
-        if self.config.Server_KEY != '':
+        if errmsg != '':
+            log.info('Starting notifying')
             self.server_chan.send(self.server_chan.form_content(sign_list, self.config.DISP_TYPE), errmsg)
+        
+        for value in sign_list:
+            if value["sign_status"] == "签到":
+                if self.config.Server_KEY != '':
+                    log.info('Starting notifying')
+                    self.server_chan.send(self.server_chan.form_content(sign_list, self.config.DISP_TYPE), errmsg)
+            break
+        
 
-        if self.config.QMsg_KEY != '':
-            self.qmesg_chan.send(self.qmesg_chan.form_content(sign_list, self.config.DISP_TYPE), errmsg)
+        # if self.config.QMsg_KEY != '':
+        #     self.qmesg_chan.send(self.qmesg_chan.form_content(sign_list, self.config.DISP_TYPE), errmsg)
